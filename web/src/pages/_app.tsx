b39853fd1f3113489f8a970a869b5ec33e3d9d0e
tomoki33kurita <tomoki33kurita@gmail.com>
2021-08-25 04:45:51 +0900

    Added chakura-ui library

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app'
 import { ReactElement } from 'react'
 import Layout from '../components/layout'
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
   )
 }
 export default MyApp
