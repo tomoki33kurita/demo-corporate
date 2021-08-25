942fe806d294b3fda55b9361b3974527f4d73528
tomoki33kurita <tomoki33kurita@gmail.com>
2021-08-25 14:49:32 +0900

    Resized fontsize

--- web/src/components/atoms/tableData.tsx
+++ web/src/components/atoms/tableData.tsx
@@ -3,7 +3,7 @@ import { Td } from '@chakra-ui/react'
 
 const TableData: React.FC = ({ children }) => {
   return (
-    <Td px={5} py={2} style={{ border: 'solid 2px #E2E8F0' }}>
+    <Td px={5} py={2} style={{ border: 'solid 2px #E2E8F0' }} fontSize={16}>
       {children}
     </Td>
   )
--- web/src/components/atoms/tableHead.tsx
+++ web/src/components/atoms/tableHead.tsx
@@ -3,7 +3,7 @@ import { Th } from '@chakra-ui/react'
 
 const TableHead: React.FC = ({ children }) => {
   return (
-    <Th px={5} py={2} style={{ border: 'solid 2px #E2E8F0' }}>
+    <Th px={5} py={2} style={{ border: 'solid 2px #E2E8F0' }} fontSize={16}>
       {children}
     </Th>
   )
