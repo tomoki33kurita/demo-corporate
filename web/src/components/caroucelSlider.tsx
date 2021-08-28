0e30f400d89d466973538f41718ae6c945cd7bd0
tomoki33kurita <tomoki33kurita@gmail.com>
2021-08-28 22:41:45 +0900

    Added react-slick

---
+++ web/src/components/caroucelSlider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import Slider from 'react-slick'
+
+export const CaroucelSlider: React.VFC = () => {
+  return (
+    <Box>
+      <Slider></Slider>
+    </Box>
+  )
+}
