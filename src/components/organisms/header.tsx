084cb9877514a75789b477f2e190c479fbeddb40
tomoki33kurita <tomoki33kurita@gmail.com>
2021-08-31 00:28:34 +0900

    Fixed display flex

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,48 +1,53 @@
-import Link from 'next/link'
-import React from 'react'
-import { DrawerMenu } from './DrawerMenu'
-import { useMediaQuery } from '@chakra-ui/media-query'
-import { Box, Flex, Link as A, List, ListItem } from '@chakra-ui/react'
-import { HoverLink } from '../atoms/hoverLink'
+import Link from "next/link";
+import React from "react";
+import { DrawerMenu } from "./DrawerMenu";
+import { useMediaQuery } from "@chakra-ui/media-query";
+import { Box, Flex, Link as A, List, ListItem } from "@chakra-ui/react";
+import { HoverLink } from "../atoms/hoverLink";
 
 const Header: React.VFC = () => {
-  const [isMobile] = useMediaQuery('(max-width:768px )')
+  const [isMobile] = useMediaQuery("(max-width:768px )");
   return (
-    <header className={'h-20'}>
+    <header className={"h-20"}>
       {isMobile ? (
-        <Flex alignItems={'center'}>
+        <Flex alignItems={"center"}>
           <DrawerMenu />
-          <Box textAlign={'center'} background={'yellow.400'} width={'full'} py={3}>
+          <Box
+            textAlign={"center"}
+            background={"yellow.400"}
+            width={"full"}
+            py={3}
+          >
             i studio
           </Box>
         </Flex>
       ) : (
         <Flex
-          width={'full'}
-          justifyContent={'space-between'}
-          borderBottom={'solid 1px'}
-          borderColor={'gray.200'}
+          width={"full"}
+          justifyContent={"space-between"}
+          borderBottom={"solid 1px"}
+          borderColor={"gray.200"}
           py={2}
         >
           <Box p={4} ml={4}>
             <Link href="/">
-              <A _hover={{ color: 'yellow.400' }}>i studio</A>
+              <A _hover={{ color: "yellow.400" }}>i studio</A>
             </Link>
           </Box>
           <Box>
             <nav>
-              <List className={'flex'}>
+              <List display={"flex"}>
                 <ListItem p={4}>
-                  <HoverLink href={'/products'}>製品</HoverLink>
+                  <HoverLink href={"/products"}>製品</HoverLink>
                 </ListItem>
                 <ListItem p={4}>
-                  <HoverLink href={'/agancy'}>お取扱店</HoverLink>
+                  <HoverLink href={"/agancy"}>お取扱店</HoverLink>
                 </ListItem>
                 <ListItem p={4}>
-                  <HoverLink href={'/about'}>会社概要</HoverLink>
+                  <HoverLink href={"/about"}>会社概要</HoverLink>
                 </ListItem>
                 <ListItem p={4}>
-                  <HoverLink href={'/contact'}>お問い合わせ</HoverLink>
+                  <HoverLink href={"/contact"}>お問い合わせ</HoverLink>
                 </ListItem>
               </List>
             </nav>
@@ -50,7 +55,7 @@ const Header: React.VFC = () => {
         </Flex>
       )}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
