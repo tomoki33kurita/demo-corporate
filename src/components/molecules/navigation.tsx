46b5fa63550621fbd0f1df0196507374c92c7d8b
tomoki33kurita <tomoki33kurita@gmail.com>
2021-08-31 00:32:57 +0900

    Added link of DrawerMenu

--- src/components/molecules/navigation.tsx
+++ src/components/molecules/navigation.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { Stack } from '@chakra-ui/react'
-import { HoverLink } from '../atoms/hoverLink'
+import React from "react";
+import { Stack } from "@chakra-ui/react";
+import { HoverLink } from "../atoms/hoverLink";
 
 type Props = {
-  onClose: () => void
-}
+  onClose: () => void;
+};
 
 export const Navigation: React.VFC<Props> = ({ onClose }) => {
   return (
-    <Stack as={'nav'} onClick={() => onClose()}>
-      <HoverLink href={'/products'}>製品ラインナップ</HoverLink>
-      <HoverLink href={'/agancy'}>お取扱店</HoverLink>
-      <HoverLink href={'/about'}>会社概要</HoverLink>
-      <HoverLink href={'/contact'}>お問い合わせ</HoverLink>
+    <Stack as={"nav"} onClick={() => onClose()}>
+      <HoverLink href={"/"}>トップ</HoverLink>
+      <HoverLink href={"/products"}>製品ラインナップ</HoverLink>
+      <HoverLink href={"/agancy"}>お取扱店</HoverLink>
+      <HoverLink href={"/about"}>会社概要</HoverLink>
+      <HoverLink href={"/contact"}>お問い合わせ</HoverLink>
     </Stack>
-  )
-}
+  );
+};
