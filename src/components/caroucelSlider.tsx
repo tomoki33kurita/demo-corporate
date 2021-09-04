627ff3e92f9bba9343b16db2ec9c98d35660d345
tomoki33kurita <tomoki33kurita@gmail.com>
2021-09-04 12:56:51 +0900

    Added Swiper library

--- src/components/caroucelSlider.tsx
+++ src/components/caroucelSlider.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Box, Image } from '@chakra-ui/react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, EffectFade } from "swiper";
+import "swiper/swiper.min.css";
+import "swiper/swiper-bundle.min.css";
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1
-}
+SwiperCore.use([Autoplay, EffectFade]);
+
 export const CaroucelSlider: React.VFC = () => {
   return (
-    <Slider {...settings}>
-      <Box>
-        <Image src={'/sampleTop.jpeg'} alt={'topImg1'} width={'100%'} />
-      </Box>
-      <Box>
-        <Image src={'/sampleTop.jpeg'} alt={'topImg1'} width={'100%'} />
-      </Box>
-    </Slider>
-  )
-}
+    <Swiper
+      pagination={{ clickable: true }}
+      slidesPerView={1}
+      autoplay={{ delay: 7000 }}
+      loop
+      effect={"fade"}
+    >
+      <SwiperSlide>
+        <Image src={"/sampleTop.jpeg"} alt={"topImg1"} width={"100%"} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={"/sampleTop2.jpeg"} alt={"topImg2"} width={"100%"} />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
---
+++ src/pages/gallery/index.tsx
@@ -0,0 +1,5 @@
+import { Box, Flex, Text, Heading } from "@chakra-ui/react";
+
+export const Gallery: React.VFC = () => {
+  return <Box pb={5}>ギャラリーページ</Box>;
+};
