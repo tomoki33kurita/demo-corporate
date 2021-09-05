f8a56600ccb50d772dfdcf861162b16edf9a0b84
tomoki33kurita <tomoki33kurita@gmail.com>
2021-09-05 20:17:10 +0900

    Added gallery link in drawer

--- src/components/molecules/navigation.tsx
+++ src/components/molecules/navigation.tsx
@@ -10,7 +10,8 @@ export const Navigation: React.VFC<Props> = ({ onClose }) => {
   return (
     <Stack as={'nav'} onClick={() => onClose()}>
       <HoverLink href={'/'}>トップ</HoverLink>
-      <HoverLink href={'/products'}>製品ラインナップ</HoverLink>
+      <HoverLink href={'/products'}>製品</HoverLink>
+      <HoverLink href={'/gallery'}>ギャラリー</HoverLink>
       <HoverLink href={'/agancy'}>お取扱店</HoverLink>
       <HoverLink href={'/about'}>会社概要</HoverLink>
       <HoverLink href={'/contact'}>お問い合わせ</HoverLink>
